47e8e5c85c8e0bda2d673c42299b69302c1c1653
Glenn Kirchner <greywolf359@gmail.com>
2017-06-15 19:51:09 -0700

    practice deploying to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
+const server = require('https').Server(app);
 const io = require('socket.io')(server);
 const moment = require('moment');
 const PORT  = process.env.PORT || 4000;
