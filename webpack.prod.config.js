b54fe4544610460df34f33e274eb88f66aa4ef6d
Glenn Kirchner <greywolf359@gmail.com>
2017-06-15 18:44:38 -0700

    practicing deploying in production mode

---
+++ webpack.prod.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	devtool: 'source-map',
+	entry: __dirname + '/src/index.js',
+	output: {
+		path: path.resolve(__dirname + '/public'),
+		filename: 'bundle.js'
+	},
+
+	plugins: [
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			minimize: true,
+			compress: {
+				warnings: false
+			}
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		})
+	],
+
+	resolve: {
+		extensions: [".js", ".jsx", ".json", ".css"]
+	},
+
+	module:{
+		rules: [
+			{
+				exclude: [
+					path.resolve(__dirname, "/node_modules")
+				]
+			},
+			{
+				test: /(\.js|.jsx$)/,
+				loader: "babel-loader",
+				options: {presets: ['env']}
+			}
+		]
+	},
+}
